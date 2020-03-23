1e73b5f064c56e0206b95d5fff5ecf182d1e8152
Arthur Sluÿters <arthur.sluyters@student.uclouvain.be>
2020-03-23 11:30:32 +0100

    Smartphone data converter

---
+++ src/dataset/SmartphoneConverter.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const fs = require('fs');
+
+const GestureSet = require('../framework/gestures/GestureSet').GestureSet;
+const GestureClass = require('../framework/gestures/GestureClass').GestureClass;
+const StrokeData = require('../framework/gestures/StrokeData').StrokeData;
+const Stroke = require('../framework/gestures/StrokeData').Stroke;
+const Point = require('../framework/gestures/Point').Point2D;
+
+
+function loadDataset(name, directory) {
+    let gestureSet = new GestureSet(name);
+    let dirPath = path.join(__dirname, directory);
+
+    fs.readdirSync(dirPath).forEach((dir) => {
+        let gestureClassDirPath = path.join(dirPath, dir);
+        let gestureClass = new GestureClass(dir);
+
+        fs.readdirSync(gestureClassDirPath).forEach((file) => {
+            let rawGesturePath = path.join(gestureClassDirPath, file);
+            let rawGestureData = JSON.parse(fs.readFileSync(rawGesturePath));
+            let gestureData = new StrokeData();
+
+            for(let i = 0 ; i < rawGestureData[0].strokes.length ; i++) {
+                let stroke = new Stroke();
+                for(var j = 0 ; j < rawGestureData[0].strokes[i].length ; j++) {
+                    let x = rawGestureData[0].strokes[i][j].x;
+                    let y = rawGestureData[0].strokes[i][j].y;
+                    let t = rawGestureData[0].strokes[i][j].t;
+                    stroke.addPoint(new Point(x,y,t));
+                }
+                gestureData.addStroke(stroke);
+            }
+            gestureClass.addSample(gestureData)
+        });
+
+        gestureSet.addGestureClass(gestureClass)
+    })
+    
+    return gestureSet;
+}
+
+module.exports = {
+    loadDataset
+};
