ccb1e1a75d8d57fe28584a63cbd9bbf0b925a93a
bnothomb <bnothomb@hotmail.com>
2020-04-22 23:22:42 +0200

    start unify guinevere dataset

--- src/framework/gestures/StrokeData.js
+++ src/framework/gestures/StrokeData.js
@@ -13,7 +13,22 @@ class StrokeData {
 
 class Stroke {
     constructor() {
+        this.paths = new Map();
+    }
+
+    addPath(label, path){
+        this.paths.set(label, path);
+    }
+
+    toJSON(clef){
+        return Array.from(this.paths.entries());
+    }
+}
+
+class Path {
+    constructor(label) {
         this.points = [];
+        this.label = label;
     }
 
     addPoint(point){
@@ -23,5 +38,6 @@ class Stroke {
 
 module.exports = {
     StrokeData,
-    Stroke
+    Stroke,
+    Path
 };
