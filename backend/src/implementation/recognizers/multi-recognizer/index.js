d83cc49e8f8a28fdeb691681360dc861473816a7
Arthur Sluÿters <arthur.sluyters@uclouvain.be>
2020-12-01 11:35:59 +0100

    Multi-recognizer with weights

--- backend/src/implementation/recognizers/multi-recognizer/index.js
+++ backend/src/implementation/recognizers/multi-recognizer/index.js
@@ -6,19 +6,54 @@ class Recognizer extends AbstractRecognizer {
   constructor(options, dataset) {
     super();
     this.recognizers = [];
-    this.weights = [];
+		this.weights = [];
+		options.recognizers.forEach(recognizer => {
+			this.recognizers.push(new recognizer.module(recognizer.moduleSettings));
+			this.weights.push(recognizer.additionalSettings.weight);
+		});
+		if (dataset !== undefined) {
+			dataset.getGestureClasses().forEach((gesture) => {
+				gesture.getSamples().forEach(sample => {
+					this.addGesture(gesture.name, sample);
+				}
+				);
+			});
+		}
 	}
 
 	addGesture(name, sample) {
-
+		this.recognizers.forEach(recognizer => {
+			recognizer.addGesture(name, sample);
+		});
 	}
 
 	removeGesture(name) {
-
+		this.recognizers.forEach(recognizer => {
+			recognizer.removeGesture(name);
+		});
 	}
 
 	recognize(sample) {
-		
+		let results = {};
+		let bestFit = '';
+		this.recognizers.forEach((recognizer, index) => {
+			let result = recognizer.recognize(sample);
+			if (result.name) {
+				let weightedScore = this.weights[index] * (result.score ? result.score : 1);
+				if (results.hasOwnProperty(result.name)) {
+					weightedScore = results[result.name] + weightedScore;
+				}
+				results[result.name] = weightedScore;
+				if (!bestFit || weightedScore > results[bestFit]) {
+					bestFit = result.name;
+				}
+			}
+		});
+		return {
+			name: bestFit,
+			time: -1, // TODO
+			score: results[bestFit]
+		};
 	}
 
 	toString() {
@@ -26,3 +61,5 @@ class Recognizer extends AbstractRecognizer {
     return `${Recognizer.name}`;
   }
 }
+
+module.exports = Recognizer;
