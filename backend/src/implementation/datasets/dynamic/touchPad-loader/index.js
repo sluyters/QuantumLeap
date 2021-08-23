1145f886d1ae0aa0cd602402f6911869ce76c4d5
RNeuville <romain.neuville@student.uclouvain.be>
2021-08-23 13:59:01 +0200

    Update touchpad loader + Dataset

--- backend/src/implementation/datasets/dynamic/touchPad-loader/index.js
+++ backend/src/implementation/datasets/dynamic/touchPad-loader/index.js
@@ -8,29 +8,33 @@ const Stroke = require('../../../../framework/gestures/stroke-data').Stroke;
 const Path = require('../../../../framework/gestures/stroke-data').Path;
 const Point3D = require('../../../../framework/gestures/point').Point3D;
 
-var user = 1
-var gestureIndex = 1
-var count = 1
-
 
 function loadDataset(name, datasetPath, identifier, sensorPointsNames){
     let gestureSet = new GestureSet(name);
     let dirPath = datasetPath;
+    let gestureIndex = 0;
     
     
 
     fs.readdirSync(dirPath, {withFileTypes: true}).filter(dirent => !dirent.isFile()).map(dirent => dirent.name).forEach((user_dir) => {
         let gestureClassDirPath = path.join(dirPath,user_dir);
-        let gestureClass = new GestureClass(user_dir,gestureIndex);
-        gestureIndex += 1;
-        user += 1;
-
 
         fs.readdirSync(gestureClassDirPath).forEach((sample) =>{
+
             let rawGesturePath = path.join(gestureClassDirPath,sample);
             let rawGestureData = JSON.parse(fs.readFileSync(rawGesturePath));
-            let gestureData = new StrokeData(user,count,undefined);
-            count += 1
+
+            let filename = sample.split(".")[0].split("-");
+            let gestureName = filename[0].split("#")[0];
+
+            let infosupp = undefined;
+            if (filename[0].split("#").length > 1) {
+                infosupp = filename[0].split("#")[1];
+            }
+            let id = filename[1];
+
+            let gestureData = new StrokeData(parseInt(user_dir),id,undefined);
+
             let string3D = "3D_"
             let string2D1 = "2DTouch1_"
             let string2D2 = "2DTouch2_"
@@ -38,19 +42,16 @@ function loadDataset(name, datasetPath, identifier, sensorPointsNames){
 
             
             let strokePath3D = new Path(string3D.concat(identifier));
-           // let strokePath3D_2 = new Path("3D2");
             let strokePath2D1 = new Path(string2D1.concat(identifier));
             let strokePath2D2 = new Path(string2D2.concat(identifier));
             let strokePath2D3 = new Path(string2D3.concat(identifier));
             gestureData.addPath(string3D.concat(identifier),strokePath3D);
-           // gestureData.addPath("3D2",strokePath3D_2);
             gestureData.addPath(string2D1.concat(identifier),strokePath2D1);
             gestureData.addPath(string2D2.concat(identifier),strokePath2D2);
             gestureData.addPath(string2D3.concat(identifier),strokePath2D3);
 
 
             let stroke3D = new Stroke(string3D.concat(identifier));
-         //   let stroke3D_2 = new Stroke("3D2");
             let stroke2D1 = new Stroke(string2D1.concat(identifier));
             let stroke2D2 = new Stroke(string2D2.concat(identifier));
             let stroke2D3 = new Stroke(string2D3.concat(identifier));
@@ -67,7 +68,6 @@ function loadDataset(name, datasetPath, identifier, sensorPointsNames){
                     stroke3D.addPoint(basic_point);
                     stroke2D2.addPoint(basic_point);
                     stroke2D3.addPoint(basic_point);
-             //       stroke3D_2.addPoint(basic_point);
                 }
                 if(value[i].type === "2DTouch2"){
                     stroke2D2.addPoint(new Point3D(value[i].x[0],value[i].y[0],0,value[i].count));
@@ -75,14 +75,12 @@ function loadDataset(name, datasetPath, identifier, sensorPointsNames){
                     stroke3D.addPoint(basic_point);
                     stroke2D1.addPoint(basic_point);
                     stroke2D3.addPoint(basic_point);
-                //    stroke3D_2.addPoint(basic_point);
 
                     stroke2D2.addPoint(new Point3D(value[i].x[1],value[i].y[1],0,value[i].count));
 
                     stroke3D.addPoint(basic_point);
                     stroke2D1.addPoint(basic_point);
                     stroke2D3.addPoint(basic_point);
-                  //  stroke3D_2.addPoint(basic_point);
                 }
                 if(value[i].type === "2DTouch3"){
                     stroke2D3.addPoint(new Point3D(value[i].x[0],value[i].y[0],0,value[i].count));
@@ -90,21 +88,18 @@ function loadDataset(name, datasetPath, identifier, sensorPointsNames){
                     stroke3D.addPoint(basic_point);
                     stroke2D1.addPoint(basic_point);
                     stroke2D2.addPoint(basic_point);
-                   // stroke3D_2.addPoint(basic_point);
 
                     stroke2D3.addPoint(new Point3D(value[i].x[1],value[i].y[1],0,value[i].count));
 
                     stroke3D.addPoint(basic_point);
                     stroke2D1.addPoint(basic_point);
                     stroke2D2.addPoint(basic_point);
-                    //stroke3D_2.addPoint(basic_point);
 
                     stroke2D3.addPoint(new Point3D(value[i].x[2],value[i].y[2],0,value[i].count));
 
                     stroke3D.addPoint(basic_point);
                     stroke2D1.addPoint(basic_point);
                     stroke2D2.addPoint(basic_point);
-                   // stroke3D_2.addPoint(basic_point);
                 }
                 else if(value[i].type === "3D"){
                 
@@ -113,7 +108,6 @@ function loadDataset(name, datasetPath, identifier, sensorPointsNames){
                     stroke2D2.addPoint(basic_point);
                     stroke2D1.addPoint(basic_point);
                     stroke2D3.addPoint(basic_point);
-                   // stroke3D_2.addPoint(basic_point);
                 }
                                 
             }
@@ -121,13 +115,22 @@ function loadDataset(name, datasetPath, identifier, sensorPointsNames){
             strokePath2D2.addStroke(stroke2D2);
             strokePath2D3.addStroke(stroke2D3);
             strokePath3D.addStroke(stroke3D);
-           // strokePath3D_2.addStroke(stroke3D_2);
 
-            gestureClass.addSample(gestureData);
+            if (gestureSet.getGestureClasses().has(gestureName)) {
+                gestureSet.getGestureClasses().get(gestureName).addSample(gestureData);
+            } else {
+                let gestureClass = new GestureClass(gestureName, gestureIndex);
+                gestureIndex += 1;
+                gestureClass.addSample(gestureData);
+                gestureSet.addGestureClass(gestureClass);
+            }
+
+
+           
         });
-        gestureSet.addGestureClass(gestureClass);
 
     })
+
     return gestureSet;
 }
 module.exports = {
